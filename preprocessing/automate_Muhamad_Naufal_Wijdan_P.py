3b94fa21c768d66931cfbc5c2245373e4b826ed4
naufalwijdan14-ai <naufalwijdan14@gmail.com>
2025-12-22 17:36:51 +0700

    Update automate_Muhamad_Naufal_Wijdan_P.py

--- preprocessing/automate_Muhamad_Naufal_Wijdan_P.py
+++ preprocessing/automate_Muhamad_Naufal_Wijdan_P.py
@@ -17,17 +17,20 @@ def preprocess_data(input_path: str, output_path: str) -> None:
     print(f"Berhasil! File disimpan di: {output_path}")
 
 if __name__ == "__main__":
-    # Lokasi folder preprocessing
+  
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     
-    # 1. Pastikan nama file ini sesuai dengan yang ada di folder GitHub kamu!
-    INPUT_PATH = os.path.join(BASE_DIR, "train.csv") 
+    ROOT_DIR = os.path.dirname(BASE_DIR)
+
+    INPUT_PATH = os.path.join(ROOT_DIR, "train.csv") 
     
-    # 2. Path output sesuai keinginanmu
     OUTPUT_PATH = os.path.join(BASE_DIR, "titanic_preprocessing.csv")
+
+    print(f"DEBUG: Mencari dataset di: {INPUT_PATH}")
 
     if os.path.exists(INPUT_PATH):
         preprocess_data(INPUT_PATH, OUTPUT_PATH)
     else:
         print(f"ERROR: File '{INPUT_PATH}' tidak ditemukan!")
-        print(f"Isi folder saat ini: {os.listdir(BASE_DIR)}")
+       
+        print(f"Isi folder ROOT: {os.listdir(ROOT_DIR)}")
